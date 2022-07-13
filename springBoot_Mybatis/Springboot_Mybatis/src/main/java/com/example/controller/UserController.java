e895eed35b7c51af6f89a174c6fa75896798caab
703864911@qq.com <703864911@qq.com>
2022-07-13 15:13:40 +0800

    eclipse-springboot-2

--- springBoot_Mybatis/Springboot_Mybatis/src/main/java/com/example/controller/UserController.java
+++ springBoot_Mybatis/Springboot_Mybatis/src/main/java/com/example/controller/UserController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSON;
+import com.example.entity.User;
 import com.example.service.IUserService;
 
 /**
@@ -31,7 +34,9 @@ public class UserController {
     @RequestMapping("getUser/{id}")
     public String GetUser(@PathVariable int id){
     	
-        return userService.Sel(id).toString();
+    	User user = userService.Sel(id);
+    	
+        return JSON.toJSONString(user);
     }
     
     
